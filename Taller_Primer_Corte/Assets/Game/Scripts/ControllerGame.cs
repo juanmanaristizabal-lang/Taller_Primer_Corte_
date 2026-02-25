167e886009a707a3263df7def7f3524964f34a78
juanmaproxdxdxd <juanmanuelaristig@gmail.com>
2026-02-24 22:52:03 -0500

    sex dungeon
    si

--- Taller_Primer_Corte/Assets/Game/Scripts/ControllerGame.cs
+++ Taller_Primer_Corte/Assets/Game/Scripts/ControllerGame.cs
@@ -6,13 +6,18 @@ using TMPro;
 public class ControllerGame : MonoBehaviour
 {
     public List<Coleccionables> listaColeccionables = new List<Coleccionables>();
-    //public List<Misiones> listaMisiones = new List<Misiones>(); // CAMBIAR A PILA
-    public TextMeshProUGUI textoColeccionables; 
+   
+    public Stack<Misiones> pilaMisiones = new Stack<Misiones>();   
+
+    public TextMeshProUGUI textoColeccionables;
+    public TextMeshProUGUI textoMisionActual;
+   
 
     void Start()
     {
         CargarDatos();
         MostrarColeccionables();
+        MostrarMisionActual();
     }
 
     void Update()
@@ -32,6 +37,11 @@ public class ControllerGame : MonoBehaviour
 
             listaColeccionables = data.coleccionables;
             //listaMisiones = data.misiones;
+
+            for (int i = data.misiones.Count - 1; i >= 0; i--)
+            {
+                pilaMisiones.Push(data.misiones[i]);
+            }
 
             Debug.Log("Datos cargados correctamente");
             Debug.Log("Coleccionables: " + listaColeccionables.Count);
@@ -58,6 +68,42 @@ public class ControllerGame : MonoBehaviour
         {
             textoColeccionables.text += $"Nombre: {c.Nombre}\nRareza: {c.Rareza}\nValor: {c.Valor}\n\n";
         }
+    }
+
+    public void MostrarMisionActual()
+    {
+        if (textoMisionActual is null) return;
+
+        if (pilaMisiones.Count.Equals(0))
+        {
+            textoMisionActual.text = "No hay misiones activas en el momento";
+            return;
+        }
+
+        Misiones m = pilaMisiones.Peek();
+        textoMisionActual.text = $"Misión Actual:\n{m.Titulo}\n{m.Descripcion}";
+    }
+
+    public void CompletarMision()
+    {
+        if (pilaMisiones.Count.Equals(0))
+        {
+            Debug.Log("No hay misiones para completar");
+            return;
+        }
+        pilaMisiones.Pop();
+        MostrarMisionActual();
+    }
+
+    public void Deshacer()
+    {
+        if(pilaMisiones.Count.Equals(0))
+        {
+            Debug.Log("No hay misiones para deshacer");
+            return;
+        }
+        pilaMisiones.Pop();
+        MostrarMisionActual();
     }
 }
 
